931843caeda394aa5a0b48d6748420cd24864833
drxzcl <drxzclx@gmail.com>
2017-02-09 23:43:00 +0100

    Store measurements in blocks
    Store measurements in blocks to cut down on datastore loads

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import json
 import pytz
 import datetime
 import random
+import time
+import logging
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -20,19 +22,52 @@ def hello():
 
 @app.route('/chart/<identifier>/<type_>')
 def chart(identifier, type_):
-    return render_template('chart.template.html', id = identifier, type = type_)
+    since = request.args.get('since', str(int(time.time())-24*60*60)) # Last 24 hrs
+    return render_template('chart.template.html', id = identifier, type = type_, since=since)
 
 
 class Measurement(ndb.Model):
-    """Models an individual Guestbook entry with content and date."""
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
     identifier = ndb.StringProperty()
     type = ndb.StringProperty()
     value = ndb.StringProperty()
 
-    # @classmethod
-    # def query_book(cls, ancestor_key):
-    #     return cls.query(ancestor=ancestor_key).order(-cls.date)
+    @classmethod
+    def last(cls,identifier,type_):
+        return cls.query(cls.identifier == identifier, cls.type == type_).order(-cls.timestamp)
+
+    @classmethod
+    def since(cls,identifier,type_, since):
+        return cls.query(cls.identifier == identifier, cls.type == type_, cls.timestamp > since).order(cls.timestamp)
+
+    @classmethod
+    def all(cls,identifier,type_):
+        return cls.query(cls.identifier == identifier, cls.type == type_).order(cls.timestamp)
+
+
+class MeasurementBlock(ndb.Model):
+    first = ndb.DateTimeProperty()
+    last = ndb.DateTimeProperty()
+    identifier = ndb.StringProperty()
+    type = ndb.StringProperty()
+    count = ndb.IntegerProperty()
+    values = ndb.TextProperty()
+
+    @classmethod
+    def since(cls,identifier,type_, count, since):
+        return cls.query(cls.identifier == identifier, cls.type == type_, cls.count == count, cls.last > since).order(cls.first)
+
+
+
+def do_publish(identifier, type_, value, ts = None):
+    indexdata(identifier, type_)
+    measurement = Measurement(identifier=identifier, type=type_, value=value)
+    if ts is not None:
+        measurement.timestamp = ts
+    measurement.put()
+
+    if random.random() < .1:
+        indexdata(identifier, type_)
 
 
 @app.route('/publish')
@@ -42,8 +77,7 @@ def publish():
     for key, value in request.args.iteritems():
         if key in ("id", "secret"):
             continue
-        measurement = Measurement(identifier=identifier, type=key, value=value)
-        measurement.put()
+        do_publish(identifier, key, value)
     return "Ok."
 
 
@@ -64,6 +98,78 @@ def format_jsdate(dt,tz):
     return "Date(%d,%d,%d,%d,%d,%d)" % (dt.year, dt.month-1, dt.day, dt.hour, dt.minute, dt.second)
 
 
+BLOCK_SIZE = 15
+BLOCK_DEPTH = 3
+
+
+def indexblocks(identifier, type_, depth):
+
+    blocks = MeasurementBlock.query(MeasurementBlock.identifier == identifier, MeasurementBlock.type == type_,
+                                    MeasurementBlock.count == BLOCK_SIZE ** depth).order(MeasurementBlock.first).fetch(keys_only=True)
+    i = 0
+    for blocknr in range(len(blocks) // BLOCK_SIZE):
+        blockdata = []
+        first, last = datetime.datetime(2999,12,31), datetime.datetime(1000,1,1)
+        for bnr in range(BLOCK_SIZE):
+            block = blocks[i].get()
+            i+=1
+            first = min(first, block.first)
+            last = max(last, block.last)
+            blockdata.extend(json.loads(block.values))
+            block.key.delete()
+
+        metablock = MeasurementBlock(identifier=identifier, type=type_, count = BLOCK_SIZE ** (depth+1),
+                                     first = first, last = last, values = json.dumps(blockdata))
+        metablock.put()
+
+    return "Ok."
+
+@app.route('/indexdata/<identifier>/<type_>')
+def indexdata(identifier, type_):
+
+    #identifier = request.args.get('id')
+    #type_ = request.args.get('type')
+
+    # Start at block depth N-1, aggregate blocks.
+    depth = BLOCK_DEPTH-1
+    count = BLOCK_SIZE ** depth
+
+    # aggregate
+    # now aggregate the individual measurements
+    measurements = Measurement.all(identifier,type_).order(Measurement.timestamp).fetch(keys_only=True)
+    i = 0
+    for blocknr in range(len(measurements) // BLOCK_SIZE):
+        blockdata = []
+        to_delete = []
+        first, last = datetime.datetime(2999,12,31), datetime.datetime(1000,1,1)
+        for mnr in range(BLOCK_SIZE):
+            measurement = measurements[i].get()
+            if not measurement:
+                return "Odd stuff."
+            i += 1
+            ts = measurement.timestamp
+            blockdata.append((int(time.mktime(ts.timetuple())),measurement.value))
+            first = min(first, ts)
+            last = max(last, ts)
+            to_delete.append(measurement.key)
+
+        # Full block, add it
+        block = MeasurementBlock(identifier=identifier, type = type_,
+                                 count = BLOCK_SIZE, first = first, last = last,
+                                 values = json.dumps(blockdata))
+        block.put()
+        for key in to_delete:
+            key.delete()
+
+    indexblocks(identifier, type_, 1)
+    indexblocks(identifier, type_, 2)
+
+    return "Ok."
+
+
+
+
+
 @app.route('/getdata')
 def getdata():
     identifier = request.args.get('id')
@@ -71,12 +177,25 @@ def getdata():
     callback = request.args.get('callback')
     tz = request.args.get('tz','UTC')
     tz = pytz.timezone(tz)
-    measurements = Measurement.query(Measurement.identifier == identifier, Measurement.type == type_).order(
-        -Measurement.timestamp).fetch(12*12*48) # 12 every hour for two days?
+    since = request.args.get('since', str(int(time.time())-24*60*60)) # Last 24 hrs
+    since = datetime.datetime.fromtimestamp(int(since), pytz.timezone('UTC'))
+
+
     result = {}
     result['cols'] = [{'id': 'A', 'label': 'time', 'type': 'datetime'},
          {'id': 'B', 'label': '%s' % type_, 'type': 'number'}, ]
     rows = []
+
+    blocks = MeasurementBlock.query(MeasurementBlock.identifier == identifier, MeasurementBlock.type == type_,
+                                    MeasurementBlock.last > since).order(MeasurementBlock.last).fetch()
+    for block in blocks:
+        logging.info('Reading from block %r' % block.key)
+        for ts, value in json.loads(block.values):
+            ts = datetime.datetime.utcfromtimestamp(ts).replace(tzinfo=pytz.timezone('UTC'))
+            if ts > since:
+                rows.append({'c': [{'v': format_jsdate(ts, tz)}, {'v': float(value)}]})
+
+    measurements = Measurement.since(identifier, type_, since).fetch()
     for measurement in measurements[::-1]:
         ts = measurement.timestamp
         ts = ts.replace(tzinfo=pytz.timezone('UTC')) # Datastore is in UTC
@@ -97,11 +216,12 @@ def insert_testdata():
     """
     now = datetime.datetime.now(pytz.timezone("UTC"))
     interval = datetime.timedelta(minutes=5)
-    number = 10
+    number = 117
     ts = now - number * interval
     while ts < now:
-        measurement = Measurement(identifier='TEST-ID', type='test', value="%.2f" % random.gauss(1,0.05), timestamp = ts.replace(tzinfo=None))
-        measurement.put()
+        #measurement = Measurement(identifier='TEST-ID', type='test', value="%.2f" % random.gauss(1,0.05), timestamp = ts.replace(tzinfo=None))
+        #measurement.put()
+        do_publish('TEST-ID', 'test', "%.2f" % random.gauss(1,0.05), ts.replace(tzinfo=None))
         ts += interval
     return "Ok."
 
