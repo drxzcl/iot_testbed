028994a6401c63e1ead3da0d08a7d47c2f10c681
drxzcl <drxzclx@gmail.com>
2017-03-07 20:20:53 +0100

    Formatting

--- alertfunctions.py
+++ alertfunctions.py
@@ -1,9 +1,7 @@
 from google.appengine.api import mail
-from google.appengine.api import app_identity
 
 
 def send_email(measurement, params):
-    sender = '{}@appspot.gserviceaccount.com'.format(app_identity.get_application_id())
     m = {'identifier': measurement.identifier, 'value': measurement.value}
-    mail.send_mail(sender=params['from'], to=params['to'], subject=params.get('subject','') % m,
-                   body=params.get('body','') % m)
+    mail.send_mail(sender=params['from'], to=params['to'], subject=params.get('subject', '') % m,
+                   body=params.get('body', '') % m)
